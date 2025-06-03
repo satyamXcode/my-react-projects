82d620109f68f1f2536607814b57c6657d2712f1
satyamXcode <jaiswalsatyam876@gmail.com>
2025-06-03 18:47:41 +0530

    Completed Section: Workflow, Pricing & FeatureSection

--- Virtual-R/src/App.jsx
+++ Virtual-R/src/App.jsx
@@ -1,9 +1,19 @@
- 
-function App() {
+import FeatureSection from "./components/FeatureSection";
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
+import Workflow from "./components/Workflow";
+import Pricing from "./components/Pricing";
 
+function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+      </div>
+      <FeatureSection />
+      <Workflow />
+      <Pricing />
     </>
   );
 }
